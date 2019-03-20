981f41ff254b62b9547c07721a7da3c567491d48
Andressa Theotônio <43258476+andressatheotonio@users.noreply.github.com>
2019-03-20 08:17:20 -0300

    Salvando um dos códigos de Sistemas Digitais 20/03

---
+++ SistemasDigitais/casca.vhd
@@ -0,0 +1,16 @@
+-- SISTEMAS DIGITAIS 20/03/2019
+
+entity casca is
+port(a,b,c: in bit;
+	  s: out bit);
+end casca;
+
+architecture miolo of casca is
+-- signals são como segmentos de fio, variáveis intermediárias para facilitar a escrita das expressões lógicas
+signal porta_and: bit;
+begin
+
+porta_and <= a and b;
+s <= porta_and xor c;
+
+end miolo;
