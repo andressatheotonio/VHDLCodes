921dfbc9cb743edb05b0c7933165b1b868f6e560
Andressa Theotônio <43258476+andressatheotonio@users.noreply.github.com>
2019-04-09 09:54:59 -0300

    Atualizando lookahead

--- SistemasDigitais/lookahead.vhd
+++ SistemasDigitais/lookahead.vhd
@@ -8,30 +8,36 @@ use ieee.std_logic_1164.all;
 entity lookahead is
 generic(n: integer := 4);
 port(cin: in std_logic;
-	  g,p: in std_logic_vector(n-1 downto 0);
-	  s: out std_logic_vector(n-1 downto 0));
+	  inA,inB: in std_logic_vector(n-1 downto 0);
+	  s: out std_logic_vector(n-1 downto 0)
+    	  cout,p,g: out std_logic);
 end lookahead;
 
 architecture lahead of lookahead is
 
 component halfadder is
-generic(n: integer := 4);
-port(a,b: in std_logic_vector(n-1 downto 0);
-     g,p: out std_logic_vector(n-1 downto 0));
+port(a,b: in std_logic;
+     g,p: out std_logic);
 end component;
 
-signal c: std_logic_vector(3 downto 0);
+signal c: std_logic_vector(n downto 0);
+signal gc: std_logic_vector(n-1 downto 0);
+signal pc: std_logic_vector(n-1 downto 0);
 
 begin
 
-c(0) <= g(0) or (p(0) and cin);
-c(1) <= g(1) or (p(1) and (g(0) or (p(0) and cin)));
-c(2) <= g(2) or (p(2) and (g(1) or (p(1) and (g(0) or (p(0) and cin)))));
-c(3) <= g(3) or (p(3) and (g(2) or (p(2) and (g(1) or (p(1) and (g(0) or (p(0) and cin)))))));
+c(0) <= gc(0) or (pc(0) and cin);
+c(1) <= gc(1) or (pc(1) and gc(0)) or (pc(1) and pc(0) and cin);
+c(2) <= gc(2) or (pc(2) and gc(1)) or (pc(2) and pc(1) and gc(0)) or (pc(2) and pc(1) and pc(0) and cin));
+c(3) <= gc(3) or (pc(3) and gc(2)) or (pc(3) and pc(2) and gc(1)) or (pc(3) and pc(2) and pc(1) and gc(0)) or (pc(3) and pc(2) and pc(1) and pc(0) and cin);
 
-s(0) <= c(0) xor p(0);
-s(1) <= c(1) xor p(1);
-s(2) <= c(2) xor p(2);
-s(3) <= c(3) xor p(3);
+g <= gc(3) or (pc(3) and gc(2)) or (pc(3) and pc(2) and gc(1)) or (pc(3) and pc(2) and pc(1) and gc(0));
+p <= (pc(3) and pc(2) and pc(1) and pc(0) and cin);
+cout <= c(3);
+
+s(0) <= c(0) xor pc(0);
+s(1) <= c(1) xor pc(1);
+s(2) <= c(2) xor pc(2);
+s(3) <= c(3) xor pc(3);
 
 end lahead;
