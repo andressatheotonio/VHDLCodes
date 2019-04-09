313ef1e56623756dee9a092965ae3f7c6d6e405f
Andressa Theotônio <43258476+andressatheotonio@users.noreply.github.com>
2019-04-09 09:00:37 -0300

    Corrigindo o halfadder

--- SistemasDigitais/halfadder.vhd
+++ SistemasDigitais/halfadder.vhd
@@ -6,24 +6,14 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity halfadder is
-generic(n: integer := 4);
-port(a,b: in std_logic_vector(n-1 downto 0);
-     g,p: out std_logic_vector(n-1 downto 0));
+port(a,b: in std_logic;
+     g,p: out std_logic);
 end halfadder;
 
 architecture hadd of halfadder is
 begin
 
-g(0) <= a(0) and b(0);
-p(0) <= a(0) xor b(0);
-
-g(1) <= a(1) and b(1);
-p(1) <= a(1) xor b(1);
-
-g(2) <= a(2) and b(2);
-p(2) <= a(2) xor b(2);
-
-g(3) <= a(3) and b(3);
-p(3) <= a(3) xor b(3);
+g <= a and b;
+p <= a xor b;
 
 end hadd;
