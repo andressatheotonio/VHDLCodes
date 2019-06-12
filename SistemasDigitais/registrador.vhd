18bda89d1117580373df7a0a974715f1639d6496
Andressa Theotônio <43258476+andressatheotonio@users.noreply.github.com>
2019-06-11 22:52:32 -0300

    Comentando o funcionamento do shiftreg

--- SistemasDigitais/registrador.vhd
+++ SistemasDigitais/registrador.vhd
@@ -2,26 +2,27 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity registrador is
-	port(entradad,clk: in std_logic;
-		  saidad: out std_logic_vector(7 downto 0));
+	port(entradad,clk: in std_logic; -- entradas do registrador
+		  saidad: out std_logic_vector(7 downto 0)); -- saída do registrador
 end registrador;
 
 architecture func of registrador is
 
-signal reg: std_logic_vector(7 downto 0) := "00000000";
+signal reg: std_logic_vector(7 downto 0) := "00000000"; -- signal que será atribuído a saída do shiftreg
 
 begin
 
-process(clk)
+process(clk) -- entrada que gatilhará o process
 begin
 
-if(clk = '0') then
-	reg(7 downto 1) <= reg(6 downto 0);
-	reg(0) <= entradad;
+if(clk = '0') then -- verificando se é uma borda de descida
+	reg(7 downto 1) <= reg(6 downto 0); -- deslocamento
+	reg(0) <= entradad; -- entrada preenchendo no vetor
 end if;
 
 end process;
 
-saidad <= reg;
+saidad <= reg; -- signal sendo atribuído a saída
+	
 
 end func;
