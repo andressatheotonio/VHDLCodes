47c40081345ba9b571017a091229ea9385e1f755
Andressa Theotônio <43258476+andressatheotonio@users.noreply.github.com>
2019-06-11 22:14:40 -0300

    Comentando o funcionamento do testbench

--- SistemasDigitais/testbenchmult.vhd
+++ SistemasDigitais/testbenchmult.vhd
@@ -1,4 +1,4 @@
--- testbench multiplicador
+-- Testbench do Multiplicador
 -- 30/05/2019
 
 library ieee;
@@ -8,28 +8,27 @@ entity testbenchmult is
 end testbenchmult;
 
 architecture func of testbenchmult is 
-
-
-component mult is 
-generic (m:integer :=4); -- bits do multiplicador
+	
+component multiplicador is -- importando o multiplicador
+generic (m:integer :=4);
 port(a,b:in std_logic_vector(m-1 downto 0);
 	  s:out std_logic_vector(2*m-1 downto 0));
 end component;
 
-signal a,b:std_logic_vector(3 downto 0);
-signal s:std_logic_vector(7 downto 0);
+signal a,b:std_logic_vector(3 downto 0); -- signals de entrada
+signal s:std_logic_vector(7 downto 0); -- signal de saída
 
 begin 
 
-x:mult port map(a,b,s);
+x:mult port map(a,b,s); -- correspondência signal/componente
 
-a <= "0010",
+a <= "0010", -- atribuindo valores a A
      "0100" after 100 ns,
 	  "0011" after 200 ns,
 	  "0011" after 300 ns,
 	  "0000" after 400 ns;
 	  
-b <= "0000",
+b <= "0000", -- atribuindo valores a B
      "0001" after 100 ns,
 	  "0111" after 200 ns,
 	  "0010" after 300 ns,
