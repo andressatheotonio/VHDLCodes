0403f80f5349c9247cf9e743b68328c41a000d86
Andressa Theotônio <43258476+andressatheotonio@users.noreply.github.com>
2019-06-11 22:28:39 -0300

    Comentando o funcionamento do latch when (T)

--- SistemasDigitais/latchwhen.vhd
+++ SistemasDigitais/latchwhen.vhd
@@ -2,20 +2,20 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity latchwhen is
-port(reset, preset, en, d: in std_logic;
-	  q: out std_logic);
+port(reset, preset, en, d: in std_logic; -- entradas
+	  q: out std_logic); -- saída q
 end latchwhen;
 
 architecture lw of latchwhen is
 
-signal state: std_logic := '0';
+signal state: std_logic := '0'; -- signal que será atribuído a saída
 
 begin
 
-state <= '0' when reset = '1' else
-			'1' when preset = '1' else
-			 d when en = '1' else state; 
+state <= '0' when reset = '1' else -- funcionamento do reset
+			'1' when preset = '1' else -- funcionamento do preset
+			 d when en = '1' else state; -- funcionamento do latch
 
-q <= state;
+q <= state; -- atribuindo o signal a saída
 
 end lw;
